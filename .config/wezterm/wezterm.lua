b02b2b3b9a3efa1dd296529c1482f2dd5c9b6a79
Love Hulterström <love.hulterstrom@cpacsystems.se>
2024-05-22 14:25:09 +0200

    Wezterm config cleanup and more:
    removed bloat and comments
    added font size
    hiding title bar

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,32 +1,14 @@
--- Pull in the wezterm API
 local wezterm = require('wezterm')
-
--- This will hold the configuration.
--- local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
 local act = wezterm.action;
 
 return {
-
 	-- For example, changing the color scheme:
 	color_scheme = 'noir',
 	front_end = 'WebGpu',
 	max_fps = 144,
-	window_background_opacity = 0.8
+	window_background_opacity = 0.8,
+	font_size = 20,
+	hide_tab_bar_if_only_one_tab = true,
+	use_fancy_tab_bar = true,
+	window_decorations = "RESIZE"
 }
-
---------------------------------------------------------------------------------------
--- --- Pull in the wezterm API
--- local wezterm = require 'wezterm'
---
--- -- This will hold the configuration.
--- local config = wezterm.config_builder()
---
--- -- This is where you actually apply your config choices
---
--- -- For example, changing the color scheme:
--- config.color_scheme = 'C64'
---
--- -- and finally, return the configuration to wezterm
--- return config
